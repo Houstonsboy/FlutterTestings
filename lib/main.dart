ca2d73e34fbb43d5f229525862a8b2f9244ecf72
Gift <giftgichuhi4138@gmail.com>
2024-02-02 22:13:16 +0300

    able to add the inboxedSongs widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,22 @@
 import 'package:flutter/material.dart';
-import 'package:jukebox/ListView.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
+import 'package:jukebox/Inbox.dart';
 import 'package:jukebox/Searchicon.dart';
+import 'package:jukebox/StoriesTemplate.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class Artist {
-  String name;
-  String power;
 
-  Artist(this.name, this.power);
+
+class MyApp extends StatefulWidget {
+ 
+  @override
+  State<MyApp> createState() => _MyAppState();
 }
 
-class MyApp extends StatelessWidget {
-  final List<Artist> artists = [
-    Artist("Chris Brown", "Dancing"),
-    Artist("Weeknd", "Singing"),
-    Artist(" the Creator", "Rapping"),
-    Artist("Tyler the Creator", "das"),
-    Artist("Tyler  Creator", "Rapping"),
-  ];
-  final List songs = [
-    "kanye West: Famous",
-    "aSap rocky: LSD",
-    "Jaden: CabinFever",
-    "Wakadinali: Marijuana",
-    "Chris Brown: Dont be Gone too Long"
-  ];
-  final List images = [
-    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR8_X5BzAVApWQdDD7qH1U8Abjao55yOLnjqA&usqp=CAU",
-    "https://i.pinimg.com/564x/12/7c/18/127c18d337018c197761539e1fdd1d15.jpg",
-    "https://i.pinimg.com/474x/e7/72/8d/e7728d7d995be2aecfe8adcc5d2e304a.jpg",
-    "https://i.pinimg.com/474x/0e/49/ca/0e49cab497fa5c819a6343de8f1337c4.jpg",
-    "https://i.pinimg.com/474x/58/65/ff/5865ff80b2fb2e497642bc8ccad545a0.jpg"
-  ];
-  final List users = ["Allan", "Gift", "Stacy", "James", "Queen"];
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -50,7 +31,8 @@ class MyApp extends StatelessWidget {
         ),
         child: Scaffold(
           backgroundColor: Colors.transparent,
-          appBar: AppBar(                          //header with title 
+          appBar: AppBar(
+            //header with title
             backgroundColor: Colors.transparent,
             title: Text(
               "Welcome to the Disco",
@@ -61,30 +43,32 @@ class MyApp extends StatelessWidget {
               ),
             ),
           ),
-          body: Column(
-            children: [
-              Card(             //Stories from ListView.dart
-                elevation: 3,
-                margin: EdgeInsets.all(10),
-                color: Colors.transparent,
-                child: Container(
-                  height: 150.0,
-                  child: ListView.builder(
-                    scrollDirection: Axis.horizontal,
-                    itemCount: images.length,
-                    itemBuilder: (context, index) {
-                      return Stories(
-                          imageUrl: images[index],
-                          songTitle: songs[index],
-                          username: users[index]);
-                    },
-                  ),
-                ),
-              ),
-              SizedBox (height: 10.0),
-              SearchIcon(),                      //searchicon.dart  Searchbar icon
-            ],
+          body: SingleChildScrollView(
+            child: Column(
+              children: [
+                StoriesTemplate(),
+                SizedBox(height: 10.0),
+                SearchIcon(),
+                SizedBox(height: 25.0), //searchicon.dart  Searchbar icon
+                Inbox(),
+              ],
+            ),
           ),
+                  bottomNavigationBar: GNav(tabs: [
+            GButton(
+              icon: Icons.home,
+              text: 'Home',
+            ),
+            GButton(
+              icon: Icons.chat,
+              text: 'Chat',
+            ),
+            GButton(
+              icon: Icons.search,
+              text: 'Kadmus',
+            )
+          ]),
+
         ),
       ),
     );
